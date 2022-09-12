53fe61f229cb54ceccc8cd5b148eb243d5ebd6e6
Andrei Danaila <andrei@creative-ones.com>
2022-09-12 09:59:35 +0300

    newest commit

--- admin/index.php
+++ admin/index.php
@@ -1,3 +1,68 @@
-<?php 
-    echo "Hello Admin";
-?>
+<html>
+
+<head>
+    <title>Food Order Website - Home Page</title>
+
+    <link rel="stylesheet" href="../css/admin.css">
+</head>
+
+<body>
+    <!-- Menu Section Start -->
+    <div class="menu text-center">
+        <div class="wrapper">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Admin </a></li>
+                <li><a href="#">Category</a></li>
+                <li><a href="#">Food</a></li>
+                <li><a href="#">Order</a></li>
+            </ul>
+        </div>
+
+    </div>
+    <!-- Menu Section End -->
+
+    <!-- Main Content Section Start -->
+    <div class="main-content">
+        <div class="wrapper">
+            <h1>Dashboard</h1>
+
+            <div class="col-4 text-center">
+                <h1>5</h1>
+                <br />
+                Categories
+            </div>
+
+            <div class="col-4 text-center">
+                <h1>5</h1>
+                <br />
+                Categories
+            </div>
+
+            <div class="col-4 text-center">
+                <h1>5</h1>
+                <br />
+                Categories
+            </div>
+
+            <div class="col-4 text-center">
+                <h1>5</h1>
+                <br />
+                Categories
+            </div>
+
+            <div class="clearfix"></div>
+        </div>
+    </div>
+    <!-- Main Content Section End -->
+
+    <!-- Footer Section Start-->
+    <div class="footer">
+        <div class="wrapper">
+            <p class="text-center">2022 All right reserved. Developed by - <a href="#"> Andrei</a></p>
+        </div>
+    </div>
+    <!-- Footer Section End-->
+</body>
+
+</html>
