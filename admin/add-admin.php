9504492bfd7cd2d7b75e4eb004e9ae4fed402cbb
Andrei Danaila <andrei@creative-ones.com>
2022-09-20 18:24:53 +0300

    connected database

--- admin/add-admin.php
+++ admin/add-admin.php
@@ -6,6 +6,13 @@
 
         <br><br>
 
+        <?php
+        if (isset($_SESSION['add'])) {
+            echo $_SESSION['add'];
+            unset($_SESSION['add']); //Removing session message
+        }
+        ?>
+
         <form action="" method="POST">
             <table class="tbl-30">
                 <tr>
@@ -54,6 +61,34 @@
         // get the data from form
         $full_name = $_POST['full-name'];
         $username = $_POST['username'];
-        echo $password = $_POST['password'];
+        $password = md5($_POST['password']); //Password encryption with md5
+
+        //sql query to save the data into database
+        $sql = "INSERT INTO tbl_admin SET 
+                full_name = '$full_name',
+                username = '$username',
+                password = '$password'
+                ";    
+        
+        //Execute query and save data in database
+        $res = mysqli_query($conn, $sql) or die(mysqli_error($conn));
+
+        //Check if data was inserted or not(query was executed successfully or not)
+        if($res==TRUE)
+        {
+            // echo 'data inserted';
+            //Create a session variable to display message
+            $_SESSION['add'] = "Admin added successfully";
+            //Redirect page to manage admin
+            header("location:".SITEURL.'admin/manage-admin.php');
+        }
+        else
+        {
+            // echo 'failed to insert data';
+            //Create a session variable to display message
+            $_SESSION['add'] = "Failed to add admin";
+            //Redirect page to manage admin
+            header("location:".SITEURL.'admin/manage-admin.php');
+        }
     }
 ?>
--- admin/manage-admin.php
+++ admin/manage-admin.php
@@ -4,10 +4,20 @@
 <div class="main-content">
     <div class="wrapper">
         <h1>Manage Admin</h1>
-        <br /><br />
+
+        <br />
+
+        <?php
+        if (isset($_SESSION['add'])) {
+            echo $_SESSION['add'];
+            unset($_SESSION['add']); //Removing session message
+        }
+        ?>
+
+        <br /><br /><br />
 
         <!-- Button to add admin -->
-        <a href="add-admin.php" class = "btn-primary">Add admin</a>
+        <a href="add-admin.php" class="btn-primary">Add admin</a>
 
         <br /><br /><br />
 
@@ -19,35 +29,43 @@
                 <th>Actions</th>
             </tr>
 
-            <tr>
-                <td>1. </td>
-                <td>Andrei</td>
-                <td>andrei</td>
-                <td>
-                    <a href="#" class = "btn-secondary">Update admin</a>
-                    <a href="#" class = "btn-danger">Delete admin</a>
-                </td>
-            </tr>
+            <?php
+            //query to get all admins
+            $sql = "SELECT * from tbl_admin";
+            //execute the query
+            $res = mysqli_query($conn, $sql);
 
-            <tr>
-                <td>2. </td>
-                <td>Andrei</td>
-                <td>andrei</td>
-                <td>
-                    <a href="#" class = "btn-secondary">Update admin</a>
-                    <a href="#" class = "btn-danger">Delete admin</a>
-                </td>
-            </tr>
+            //check whether the query is executed or not
+            if ($res == true) {
+                //count rows to check if we have data or not
+                $count = mysqli_num_rows($res);
 
-            <tr>
-                <td>3. </td>
-                <td>Andrei</td>
-                <td>andrei</td>
-                <td>
-                    <a href="#" class = "btn-secondary">Update admin</a>
-                    <a href="#" class = "btn-danger">Delete admin</a>
-                </td>
-            </tr>
+                if ($count > 0) {
+                    while ($rows = mysqli_fetch_assoc($res)) {
+                        $id = $rows['id'];
+                        $full_name = $rows['full_name'];
+                        $username = $rows['username'];
+
+            ?>
+
+                        <tr>
+                            <td><?php echo $id; ?> </td>
+                            <td><?php echo $full_name; ?></td>
+                            <td><?php echo $username; ?></td>
+                            <td>
+                                <a href="#" class="btn-secondary">Update admin</a>
+                                <a href="#" class="btn-danger">Delete admin</a>
+                            </td>
+                        </tr>
+
+            <?php
+                    }
+                } else {
+                }
+            }
+            ?>
+
+
         </table>
 
     </div>
